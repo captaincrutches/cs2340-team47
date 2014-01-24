32395fbeb786e0e9d984f3ec762adb6ddc88b534
Aziz Somani <asomani7@gatech.edu>
2014-01-23 21:08:54 -0500

    Added person6 part to antlabmain.java
    Added the person 6 part in the file and filled in my information.

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,6 +75,10 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
+		  //Person6 replace P6 with your name
+		  //and gburdell3 with your gt id
+		  Person6 p6 = new Person6("Aziz Somani");
+		  toPrint += p6.toString("902860225");
 		  
         System.out.println(toPrint);
         
