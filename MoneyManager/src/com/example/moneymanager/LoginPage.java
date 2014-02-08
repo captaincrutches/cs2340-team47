acadaf92da55cb0f66ca119201764b2bbbba3672
Aziz Somani <asomani7@gatech.edu>
2014-02-07 19:58:37 -0500

    Added the functionality to check if  the login credentials are valid.
    It only works right now where the username is admin and the password is
    password

--- MoneyManager/src/com/example/moneymanager/LoginPage.java
+++ MoneyManager/src/com/example/moneymanager/LoginPage.java
@@ -1,8 +1,13 @@
 package com.example.moneymanager;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginPage extends Activity {
 
@@ -17,6 +22,36 @@ public class LoginPage extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login_page, menu);
 		return true;
+	}
+	
+	public void authenticateLogin(View view)
+	{
+		EditText username = (EditText) findViewById(R.id.username);
+		EditText password = (EditText) findViewById(R.id.password);
+		boolean valid = doLoginCredentialsMatch(username, password);
+		if (valid)
+		{
+			Intent intent = new Intent(this, MyAccounts.class);
+			startActivity(intent);
+		}
+		else
+		{
+			TextView errorMessage = (TextView) findViewById(R.id.login_failed);
+			errorMessage.setTextColor(Color.RED);
+		}
+	}
+	
+	private boolean doLoginCredentialsMatch(EditText username, EditText password)
+	{
+		// This will be replaced with getting information from the database and actually checking it
+		if (username.getText().toString().equals("admin") && password.getText().toString().equals("password"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 }
