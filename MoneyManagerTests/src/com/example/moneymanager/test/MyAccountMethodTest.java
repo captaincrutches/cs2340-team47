d96f958a890ee46a2d97e0fed11e2177fa3fd0b2
Captain Crutches <captaincrutches@gmail.com>
2014-03-31 18:10:57 -0400

    Added an Android test project

---
+++ MoneyManagerTests/src/com/example/moneymanager/test/MyAccountMethodTest.java
@@ -0,0 +1,11 @@
+package com.example.moneymanager.test;
+
+import org.junit.Assert;
+
+import android.test.AndroidTestCase;
+
+public class MyAccountMethodTest extends AndroidTestCase {
+	public void testTesting() {
+		Assert.fail("Test-Ception!");
+	}
+}
